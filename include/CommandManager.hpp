3c5c8a3d268c41ad967049e704b1df740256c2ab
SalamoneQ649 <salamoneq.sq@gmail.com>
2026-08-04 10:56:15 +0000

    Aug 4, 2026, 12:56 PM

--- include/CommandManager.hpp
+++ include/CommandManager.hpp
@@ -50,6 +50,8 @@ private:
             }
             std::cout << UI::CYAN << "[BT] Pobieranie pakietu '" << args[0] << "' z Twojej domeny...\n" << UI::RESET;
         });
-        registerCommand("")
+        registerCommand("exit","quit", [](const std::vector<std::string>& args)){
+            
+        }
     }
 };
